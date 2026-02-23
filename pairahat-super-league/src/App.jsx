1c322844edadefdb7b0d3df7e2a22efb34ad6a2c
MD Razu Ahamad <mdrazuahamad8@gmail.com>
2026-02-24 02:30:38 +0600

    magor-done

--- pairahat-super-league/src/App.jsx
+++ pairahat-super-league/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Players from "./pages/Players";
+import Schedule from "./pages/Schedule";
+import PlayerProfile from "./pages/PlayerProfile";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className='flex flex-col min-h-screen'>
+        <Navbar />
+
+        {/* Main Content */}
+        <div className='flex-grow'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/players' element={<Players />} />
+            <Route path='/schedule' element={<Schedule />} />
+            <Route path='/player/:id' element={<PlayerProfile />} />
+          </Routes>
+        </div>
+
+        {/* Footer always at bottom */}
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ pairahat-super-league/src/data/players.js
@@ -0,0 +1,58 @@
+import image1 from "../assets/images/MUNNA.jpg";
+
+const player = [
+  {
+    id: 1,
+    name: "Ferdaus Munna",
+    age: 30,
+    position: "Defender",
+    jerseyNumber: 9,
+    preferredFoot: "Right",
+    height: "6ft",
+    nationality: "Bangladesh",
+    image: image1,
+  },
+  {
+    id: 2,
+    name: "Karim Sheikh",
+    age: 24,
+    position: "Midfielder",
+    preferredFoot: "Left",
+    height: "5 ft 10 in",
+    nationality: "Bangladesh",
+    image: "https://i.pravatar.cc/600?img=12",
+  },
+  {
+    id: 3,
+    name: "Jamal Hossain",
+    age: 21,
+    position: "Defender",
+    preferredFoot: "Right",
+    height: "6 ft",
+    nationality: "Bangladesh",
+    image: "https://i.pravatar.cc/600?img=13",
+  },
+  {
+    id: 4,
+    name: "Sabbir Ahmed",
+    age: 23,
+    position: "Forward",
+    preferredFoot: "Right",
+    height: "5 ft 9 in",
+    nationality: "Bangladesh",
+    image: "https://i.pravatar.cc/600?img=14",
+  },
+  {
+    id: 5,
+    name: "Rifat Islam",
+    age: 25,
+    position: "Midfielder",
+    team: "Pairahat Warriors",
+    preferredFoot: "Left",
+    height: "5 ft 11 in",
+    nationality: "Bangladesh",
+    image: "https://i.pravatar.cc/600?img=15",
+  },
+];
+
+export default player;
